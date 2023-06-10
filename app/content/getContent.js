80a53e65e94784c996cf0bad04b31162ea016455
Jacob Duvander <duvan.der@live.se>
2023-06-10 12:51:17 +0200

    changed to take path from window

--- app/content/getContent.js
+++ app/content/getContent.js
@@ -4,15 +4,17 @@ import content from './sv.json'
 /*Returns the correct content depending on path */
 export function getContent(path) {
 
-    if(!path) {
+    const currentPath = path ? path : window?.location?.pathname?.substring(1)
+
+    if(!currentPath) {
         return content.pages.landingPage
     }
 
-    if(typeof content.pages[path] === "undefined" || !content.pages[path]) {
+    if(typeof content.pages[currentPath] === "undefined" || !content.pages[currentPath]) {
         return false
     }
 
-    return content.pages[path.toLowerCase()]
+    return content.pages[currentPath.toLowerCase()]
 }
 
 export function getAllPaths() {
