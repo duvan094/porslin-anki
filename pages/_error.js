9409d5854794e2826b55fd2823190b7a65ce0b0b
Jacob Duvander <duvan.der@live.se>
2023-06-10 14:47:16 +0200

    small fix

--- pages/_error.js
+++ pages/_error.js
@@ -22,7 +22,7 @@ function Error(message) {
    
   Error.getInitialProps = ({ res, error }) => {
     const statusCode = res ? res.statusCode : error ? error.statusCode : 404
-    return { statusCode, res }
+    return { statusCode }
   }
    
   export default Error
