2cb33454110071cdf0de4c9671812649199d3c21
Jacob Duvander <duvan.der@live.se>
2023-06-10 16:23:04 +0200

    added masonry grid

--- app/components/imageGroup.js
+++ app/components/imageGroup.js
@@ -5,11 +5,24 @@ import styles from './mainContent.module.css'
 
 export default function ImageGroup(props) {
     
+    const columnAmount = props.images.length >= 3 ? props.images.length / 3 : props.images.length / props.images.length
+    const imageColumns = []
+    for (let i = 0; i < props.images.length; i += columnAmount) {
+        const imageColumn = props.images.slice(i, i + columnAmount);
+        imageColumns.push(imageColumn)
+    }
+
     const images = props.images && (
         <div className={ styles.images }>
-            { props.images.map((image) =>
-                <LazyLoadImage key={ image.src } image={image} />
-            )}
+            {
+                imageColumns.map((imageColumn,index) => (
+                    <div className={styles.imageColumn} key={index}>
+                        { imageColumn.map((image) =>
+                            <LazyLoadImage key={ image.src } image={image} />
+                        )}
+                    </div>
+                ))
+            }
         </div>
     )
 
