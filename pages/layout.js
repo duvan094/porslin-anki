b69d366e3a1315d69c830cf7ef616a461264a4a2
Jacob Duvander <duvan.der@live.se>
2023-06-11 20:23:10 +0200

    some changes

---
+++ pages/layout.js
@@ -0,0 +1,23 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import Header from '../components/header'
+import Footer from '../components/footer'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Porslinsmålning',
+  description: 'En sida om porslinsmålning. Innehåll av Ann-Christine Duvander.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header></Header>  
+        {children}
+        <Footer></Footer>  
+      </body>
+    </html>
+  )
+}
