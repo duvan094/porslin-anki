62455d7c3413a74a89336b6f8b7fcf1bb02f3dd3
Jacob Duvander <duvan.der@live.se>
2023-06-11 15:33:09 +0200

    added error

---
+++ app/error.js
@@ -0,0 +1,15 @@
+'use client'
+
+import NotFound from './components/notFound'
+
+export default function Page({ params }) {
+
+    return (
+      <>
+        {
+          <NotFound></NotFound>
+        }
+        
+      </>
+    )
+}
