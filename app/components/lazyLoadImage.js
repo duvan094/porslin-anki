b235911dd691ef8866e23be105ab14dc1de0b6f8
Jacob Duvander <duvan.der@live.se>
2023-06-11 13:28:27 +0200

    AI upscaled images

--- app/components/lazyLoadImage.js
+++ app/components/lazyLoadImage.js
@@ -1,34 +1,43 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import * as NextImage from 'next/image'
 import styles from './mainContent.module.css'
 
 export default function LazyLoadImage({image}) {
 
     const [loaded, setLoaded] = useState(false)
-    const [width, setWidth] = useState(200)
-    const [height, setHeight] = useState(150)
 
+  const onLoad = () => {
+    setLoaded(true)
+  }
 
-    const isLoaded = (img) => {
-      setLoaded(true)
-      setWidth(img.naturalWidth)
-      setHeight(img.naturalHeight)
-    }
+    useEffect(()=>{
+      const img = new Image()
+      console.log(img)
+
+      img.onload = () => {
+        console.log('loaded! 1')
+        setLoaded(true)
+      }
+      
+      img.src = image.src
+    },[])
 
     return (
       <>
         <div className={styles.imageWrapper}>
-          <div className={`${styles.imageContainer} ${loaded ? styles.loaded : ''}`}>
-                <NextImage
+          {/* <div className={`${styles.imageContainer} ${loaded ? styles.loaded : ''}`}> */}
+          <div className={`${styles.imageContainer} ${styles.loaded}`}>
+                <img src={'/images/'+ image?.src} alt={image?.alt} onLoad={onLoad}></img>
+                {/* <NextImage
                     src={'/images/'+ image?.src}
                     alt={image?.alt}
                     width={width}
                     height={height}
                     onLoadingComplete={(img) => isLoaded(img)}
                     loading="lazy"
-                />
+                /> */}
             </div>
         </div>
       </>
