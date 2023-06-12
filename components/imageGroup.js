b75b966e3a45c03ecfc8ee53ec9e9765022c2659
Jacob Duvander <duvan.der@live.se>
2023-06-12 13:36:08 +0200

    removed log

--- components/imageGroup.js
+++ components/imageGroup.js
@@ -4,12 +4,8 @@ import LazyLoadImage from './lazyLoadImage'
 import styles from './imageGroup.module.css'
 
 export default function ImageGroup(props) {
-    
-    // const columnAmountMobile = props.images.length >= 2 ? Math.round(props.images.length / 2) : props.images.length / props.images.length
-    // const columnAmountDesktop = props.images.length >= 3 ? Math.round(props.images.length / 3) : props.images.length / props.images.length
 
     const calculateImagesPerColumn = (images, columnAmount) => {
-        const imageColumns = []
         const columns = new Array(columnAmount)
         for(let i = 0; i < columns.length; i++) {
             columns[i] = []
@@ -20,19 +16,11 @@ export default function ImageGroup(props) {
             for(let j = 0; j < columns.length; j++) {
 
                 if(i % columnAmount === j) {
-                    // console.log('columns', columns[j])
                     columns[j].push(images[i])
                 }
             }
         }
 
-        console.log('columns', columns)
-
-
-        // for (let i = 0; i < images.length; i += columnAmount) {
-        //     const imageColumn = images.slice(i, i + columnAmount);
-        //     imageColumns.push(imageColumn)
-        // }
         return columns
     }
 
